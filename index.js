55345b11c6084891964929b1db8859bb79ea4530
Khuram Niaz <knshaikh@gmail.com>
2017-12-13 00:01:43 -0700

    nodemon

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(express.static('public'));
-// app.use(express.static('views'));
+app.use(express.static(`${__dirname}public`));
+app.use(express.static(`${__dirname}views`));
 
 const Routes = require('./routes/route');
 
 app.get('/', (req, res) => {
-  res.send('Hello from root route (index.js)');
+  res.send('Hello from root route (index)');
 });
 
 app.use('/track', Routes); // all the routes to start from /todo/...
